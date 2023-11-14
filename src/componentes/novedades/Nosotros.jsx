16c62ff385d73ca95f88868af35f0d074af105e8
adriel2323 <adrielcolombo23@gmail.com>
2023-11-14 15:54:25 -0300

    imagen nosotros redondeo

--- src/componentes/novedades/Nosotros.jsx
+++ src/componentes/novedades/Nosotros.jsx
@@ -20,9 +20,9 @@ const Nosotros =( )=>{
 
 export default Nosotros;
 
-<div className="flex flex-col lg:flex-row w-full h-200">
+<div className="flex flex-col  w-full h-200">
                 <div className="w-full lg:w-1/2 ">
-                    <img className="w-full h-full object-cover object-right-top" src="../../../public/imagenes/general.jpeg" alt="" />
+                    <img className="w-full h-full object-cover object-right-top rounded-full shadow-md" src="../../../public/imagenes/general.jpeg" alt="" />
                 </div>
                 <p className="w-full lg:w-1/2 mt-10 text-center lg:mt-0 lg:ml-10 lg:text-right" >
                 Sanatorio de alta complejidad ubicado estratégicamente en el corazón de la ciudad. Equipado con tecnología de vanguardia de las mejores marcas y personal de profesionales capacitados para brindar el mejor servicio.
