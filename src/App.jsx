a830e53825e6868aee5efd13231480834720f251
adriel2323 <97199588+adriel2323@users.noreply.github.com>
2023-11-14 09:47:41 -0300

    Update App.jsx from servidor

--- src/App.jsx
+++ src/App.jsx
@@ -1,95 +1,4 @@
-import { useState, createContext, useEffect } from 'react'
-
-import HomePrestadores from './Pages/HomePrestadores'
-import HomeGeneral from './Pages/HomeGeneral'
-import HomePacientes from './Pages/HomePacientes'
-import HomeRH from './Pages/HomeRH'
-import HomeOs from './Pages/HomeOs' 
-import FormularioCV from './componentes/RRHH/FormularioCV'
-import BuscarCV from './componentes/RRHH/BuscarCV'
-import PaginaGestionNov from './componentes/Secciones/FormulariosNovedades/PaginaGestionNov'
-import Declaracion from "./componentes/RRHH/DeclaracionJurada/Declaracion"
-import ArchivosU from './componentes/visualizadorArchivos/ArchivoU'
-import Afiliaciones from './componentes/ObraSocial/Afiliaciones'
-import { Routes,Route} from 'react-router-dom';
-import NotFound from './Pages/NotFound'
-
-export const PerfilContext= createContext();
-
-function App() {
-  const [CurrentPath, setCuerrentPath]= useState(window.location.pathname)
-  const [login,setLogin]= useState(false);
-  const [permisos,setPermisos]=useState({});
-  const[usuario,setUsuario]=useState({algo:"algo"});
-
-  const [verModalNovedad,setVerModalNovedad]=useState(false)
-  const [novedad,setNovedad]=useState();
-  const [isLoadNovedad,setIsLoadNovedad]=useState(false);
-
-  //Gestion de permisos de usuarios
-  const [permisosPrestadores,setPermisosPrestadores]=useState(false);
-  const [permisosRrhh,setPermisosRrhh]=useState(false);
-  const [permisosPersonal,setPermisosPersonal]=useState(false);
-  const [permisosAdministradorTotal,setPermisosAdministradorTotal]=useState(false);
-  const [permisosAdministradorPersonal,setPermisosAdministradorPersonal]=useState(false);
-  const [permisosOsAdmin, setPermisosOsAdmin]= useState(false);
-
-
-  const [cartillaOpen, setCartillaOpen]= useState(false)
-  const abrirCerrarCartilla=()=>{
-        setCartillaOpen(!cartillaOpen);
-    }
-
-
-  const quitarPermisos=()=>{
-    setPermisosAdministradorTotal(false);
-    setPermisosAdministradorPersonal(false);
-    setPermisosPrestadores(false);
-    setPermisosRrhh(false);
-    setPermisosPersonal(false);
-  }
-
-  useEffect(()=>{
-    if(usuario!=undefined){
-
-      if(usuario.tipoUser===1){
-        setPermisosAdministradorTotal(true);
-        setPermisosAdministradorPersonal(true);
-        setPermisosPrestadores(true);
-        setPermisosRrhh(true);
-        setPermisosPersonal(true);
-        setPermisosOsAdmin(true);
-      }
-      else if(usuario.tipoUser===2){
-        setPermisosAdministradorPersonal(true);
-        setPermisosPrestadores(true);
-        setPermisosRrhh(true);
-        setPermisosPersonal(true);
-      }
-      else if(usuario.tipoUser===3){
-        setPermisosRrhh(true);
-      }
-      else if(usuario.tipoUser===4){
-        setPermisosPersonal(true);
-      } else if (usuario.tipoUser===5){
-        setPermisosOsAdmin(true);
-      }
-    }
-    
-  },[login])
-  useEffect(()=>{
-    if(localStorage.user!=undefined){
-      setUsuario(localStorage.user);
-      if(usuario!=undefined){
-  
-        if(usuario.tipoUser===1){
-          setPermisosAdministradorTotal(true);
-          setPermisosAdministradorPersonal(true);
-          setPermisosPrestadores(true);
-          setPermisosRrhh(true);
-          setPermisosPersonal(true);
-        }
-        else if(usuario.tipoUser===2){
+    else if(usuario.tipoUser===2){
           setPermisosAdministradorPersonal(true);
           setPermisosPrestadores(true);
           setPermisosRrhh(true);
@@ -105,7 +14,7 @@ function App() {
         }
       }
     }
-    
+
 
   },[])
 
@@ -118,7 +27,7 @@ function App() {
   return (
     <main className='text-mygray general '>
     <PerfilContext.Provider value={{
-          abrirCerrarCartilla,permisosOsAdmin, setPermisosOsAdmin, cartillaOpen, login, setLogin, usuario, setUsuario, permisos,setPermisos, permisosAdministradorPersonal, setPermisosAdministradorPersonal,setPermisosAdministradorTotal,setPermisosPersonal,setPermisosPrestadores,setPermisosRrhh,permisosAdministradorTotal,permisosPersonal,permisosPrestadores,permisosRrhh, quitarPermisos
+          abrirCerrarCartilla,permisosOsAdmin, setPermisosOsAdmin, cartillaOpen, login, setLogin, usuario, setUsuario, permisos,setPermisos, permisosAdministradorPersonal, setPermisosAdministradorPersonal,setPermisosAdministradorTotal,s>
         }}>
         <Routes>
             <Route path='/' element={<HomeGeneral usuarios= {"general"} />} />
@@ -134,17 +43,12 @@ function App() {
             <Route path='/admin/nov' element={<PaginaGestionNov usuarios= {"general"}/>} />
             <Route path='*' element={<NotFound usuarios= {"general"}/>} />
         </Routes>
-      
     </PerfilContext.Provider>
 
-      
+
 
     </main>
   )
 }
 
 export default App
-
-
-
-
